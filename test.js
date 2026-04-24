ba1483f3a88cf1d686261b016ea3c210aed9fd5e
+-*/ <dr_liu@sto-tech.hk>
2026-04-24 14:23:20 +0800

    feat: add test

---
+++ test.js
@@ -0,0 +1,34 @@
+// 示例:
+// 给定 nums = [2, 7, 11, 15], target = 9
+// 因为 nums[0] + nums[1] = 2 + 7 = 9
+// 所以返回 [0, 1]
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {}
+console.log(twoSum([2, 7, 11, 15], 9))
+
+// 示例 1:
+
+// 输入: "()"
+// 输出: true
+// 示例 2:
+// 输入: "()[]{}"
+// 输出: true
+// 示例 3:
+// 输入: "(]"
+// 输出: false
+// 示例 4:
+// 输入: "([)]"
+// 输出: false
+// 示例 5:
+// 输入: "{[]}"
+// 输出: true
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {}
+console.log(isValid('({})'))
